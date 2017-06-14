086d10916862337bb3af8d1293fe49e338b5b02d
roxmaddye <roxanem@yahoo.com>
2017-06-14 11:46:13 +0800

    Add files via upload

---
+++ src/utils/Constant.java
@@ -0,0 +1,17 @@
+package com.ruanko.utils;
+
+public class Constant {
+	public static final int STATE_DRAFTED = 1;			
+	public static final int STATE_CSIGNED = 2;			
+	public static final int STATE_FINALIZED = 3;		
+	public static final int STATE_APPROVED = 4;			
+	public static final int STATE_SIGNED = 5;			
+	
+	public static final int PROCESS_CSIGN = 1;			
+	public static final int PROCESS_APPROVE = 2;		
+	public static final int PROCESS_SIGN = 3;			
+	
+	public static final int UNDONE = 0;					
+	public static final int DONE = 1;					
+	public static final int VETOED = 2;
+}
